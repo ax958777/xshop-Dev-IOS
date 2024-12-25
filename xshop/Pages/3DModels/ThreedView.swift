5f9a448e9006bbd9f776e0354ff66d4da1ff51fb
ax958777 <ax958777@gmail.com>
2024-12-25 14:36:09 -0700

    initial

---
+++ xshop/Pages/3DModels/ThreedView.swift
@@ -0,0 +1,18 @@
+//
+//  ThreedView.swift
+//  xshop
+//
+//  Created by AlexX on 2024-12-23.
+//
+
+import SwiftUI
+
+struct ThreedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreedView()
+}
